65c40e4b9f8a329a8f96fe9974d0e9942cf9e0f1
OLEKSANDR VEREMIIENKO <120383111+SkyKeyUA@users.noreply.github.com>
2024-12-24 20:53:14 -0500

    Delete lesson_20 directory

--- lesson_20/lesson_20/js/script.js
+++
@@ -1,7 +0,0 @@
-if (1099 >= window.innerWidth) {
-  const menuToggle = document.querySelector('.icon-menu');
-
-  menuToggle?.addEventListener('click', () => {
-    document.documentElement.classList.toggle('open-menu');
-  });
-}
